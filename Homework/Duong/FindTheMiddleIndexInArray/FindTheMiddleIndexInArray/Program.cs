493b901bb95149ded0c246414300e4b6f101cacb
tyu32 <42110387+tyu32@users.noreply.github.com>
2023-07-30 14:31:08 -0700

    Baby's homework

---
+++ Homework/Duong/FindTheMiddleIndexInArray/FindTheMiddleIndexInArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FindTheMiddleIndexInArray
+{
+    public class Solution
+    {
+        public static int FindMiddleIndex(int[] nums)
+        {
+            //Add your code here
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {2, 3, -1, 8, 4};
+
+            int result = FindMiddleIndex(nums);
+
+            Console.WriteLine("Result is: " + result);
+        }
+    }
+}
+
