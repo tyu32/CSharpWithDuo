d480ca96929c104bc9327bd92d224b6e94f99aa1
tyu32 <42110387+tyu32@users.noreply.github.com>
2023-08-12 17:11:19 -0700

    Add start code

--- Homework/Duong/LeftRightDifference/LeftRightDifference/LeftRightDifference/Program.cs
+++ Homework/Duong/LeftRightDifference/LeftRightDifference/LeftRightDifference/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LeftRightDifference
+{
+    public class Solution
+    {
+        public static int[] LeftRightDifference(int[] nums)
+        {
+            int[] answer = new int[nums.Length];
+            //Add your code here
+
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 10, 4, 8, 3 };
+
+            int[] result = LeftRightDifference(nums);
+
+            int[] expectResult = new int[] { 15, 1, 11, 22 };
+
+            for (int i = 0; i < Math.Min(result.Length, expectResult.Length); ++i)
+            {
+                Console.WriteLine("Expect index: " + i + " is " + expectResult[i]);
+                Console.WriteLine("Actually index: " + i + " is " + result[i]);
+            }
+
+            Console.WriteLine("Result is: " + result);
+        }
+    }
+}
 
